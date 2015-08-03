25d619f7f052432e982b7c6adb94f2c830230c8e
Stephon Patton <stephonpatton@icloud.com>
2015-08-03 10:17:51 -0400

    Shifting of two pixels
    Technically this part of the project is done, but it isn’t exactly how
    I wanted the image to turn out. Working on a different approach

--- pixelManipulator/Main.java
+++ pixelManipulator/Main.java
@@ -1,16 +1,21 @@
 package pixelManipulator;
+import java.awt.Color;
 import java.awt.Image.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.PixelGrabber;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
+
 import javax.imageio.ImageIO;
 
 public class Main {
 	public static void main(String[] args) {
 		BufferedImage image = null;
+		File outputFile = null;
 		try {
 			image = ImageIO.read(new File("robot.jpg"));
+			outputFile = new File("saved.png");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -29,10 +34,40 @@ public class Main {
 				greenArray[x] = green;
 				blue = imageRGB & 0x000000ff;
 				blueArray[x] = blue;
-				System.out.println(x + " " + x);
-				System.out.println("Red is " + red);
-				System.out.println("Green is " + green);
-				System.out.println("Blue is " + blue);
+//				System.out.println(x + " " + x);
+//				System.out.println("Red is " + red);
+//				System.out.println("Green is " + green);
+//				System.out.println("Blue is " + blue);
+			}
+		Random rand = new Random();
+	
+		int pixelOne, pixelTwo, pixelThree;
+		for(int i = 0; i<10000; i++) {	
+			int randomX1 = rand.nextInt(imageWidth);
+			int randomY1 = rand.nextInt(imageWidth);
+			int randomX2 = rand.nextInt(imageWidth);
+			int randomY2 = rand.nextInt(imageWidth);
+			
+			pixelOne = image.getRGB(randomX1, randomY1);
+			int pixelOneRed = (pixelOne & 0x00ff0000) >> 16;
+			int pixelOneGreen = (pixelOne & 0x00000ff00) >> 8;
+			int pixelOneBlue = pixelOne & 0x000000ff;
+			
+			pixelTwo = image.getRGB(randomX2, randomY2);
+			int pixelTwoRed = (pixelTwo & 0x00ff0000) >> 16;
+			int pixelTwoGreen = (pixelTwo & 0x00000ff00) >> 8;
+			int pixelTwoBlue = pixelTwo & 0x000000ff;
+			
+			int rgb = new Color(pixelOneRed, pixelOneGreen, pixelOneBlue).getRGB();
+			int rgb2 = new Color(pixelTwoRed, pixelTwoGreen, pixelTwoBlue).getRGB();
+			image.setRGB(randomX2, randomY2, rgb);
+			image.setRGB(randomX1, randomY1, rgb2);
+
+		}	
+		try {
+				ImageIO.write(image, "jpeg", outputFile);
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 	}
 }
