39fd212af5abeac151b47cf4da062bbff3d1d36c
Stephon Patton <stephonpatton@icloud.com>
2015-08-03 10:19:57 -0400

    Deletion
    Deleted the unnecessary code that was only used to get the RGB values
    of every pixel.

--- pixelManipulator/Main.java
+++ pixelManipulator/Main.java
@@ -23,22 +23,22 @@ public class Main {
 		int imageWidth = image.getWidth();
 		int imageHeight = image.getHeight();
 		int imageRGB = 0, red = 0, green =0, blue = 0;
-		int[] redArray = new int[imageWidth];
-		int[] greenArray = new int[imageWidth];
-		int[] blueArray = new int[imageWidth];
-			for(int x = 0; x<imageWidth; x++) {	
-				imageRGB = image.getRGB(x, x);
-				red = (imageRGB & 0x00ff0000) >> 16;
-				redArray[x] = red;
-				green = (imageRGB & 0x00000ff00) >> 8;
-				greenArray[x] = green;
-				blue = imageRGB & 0x000000ff;
-				blueArray[x] = blue;
-//				System.out.println(x + " " + x);
-//				System.out.println("Red is " + red);
-//				System.out.println("Green is " + green);
-//				System.out.println("Blue is " + blue);
-			}
+//		int[] redArray = new int[imageWidth];
+//		int[] greenArray = new int[imageWidth];
+//		int[] blueArray = new int[imageWidth];
+//			for(int x = 0; x<imageWidth; x++) {	
+//				imageRGB = image.getRGB(x, x);
+//				red = (imageRGB & 0x00ff0000) >> 16;
+//				redArray[x] = red;
+//				green = (imageRGB & 0x00000ff00) >> 8;
+//				greenArray[x] = green;
+//				blue = imageRGB & 0x000000ff;
+//				blueArray[x] = blue;
+////				System.out.println(x + " " + x);
+////				System.out.println("Red is " + red);
+////				System.out.println("Green is " + green);
+////				System.out.println("Blue is " + blue);
+//			}
 		Random rand = new Random();
 	
 		int pixelOne, pixelTwo, pixelThree;
